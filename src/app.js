cc36d818bb821bf8569f436ebd64779ba048591e
Uzair Baluch <uzairbaluchdev@gmail.com>
2026-03-11 14:40:22 -0700

    commited imports of swagger

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import userRouter from "./routes/user.routes.js";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./config/swagger.js";
+// import swaggerUi from "swagger-ui-express";
+// import { swaggerSpec } from "./config/swagger.js";
 
 const app = express();
 
@@ -32,7 +32,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/v1/users", userRouter);
 

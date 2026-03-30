719a82948bdca4560098c2f58261393627f5a0bf
Uzair Baluch <uzairbaluchdev@gmail.com>
2026-03-31 00:33:28 +0400

    Refactor multer middleware for file uploads: Updated filename generation to include a timestamp and random number for uniqueness, enhancing file management. Added path import for better extension handling.

--- src/middlewares/multer.middelware.js
+++ src/middlewares/multer.middelware.js
@@ -1,14 +1,15 @@
+import path from "path";
 import multer from "multer";
 
-
 const storage = multer.diskStorage({
-    destination:  function (req,file,cb){
-        cb(null, "./public/temp")
-    },
-    filename: function (req,file,cb){
-        cb(null, file.originalname)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`);
+  },
+});
 export const upload = multer({
-    storage
-})
+  storage,
+});
